da9b067705919c90c4917569548a9d27472b7632
Saiful <saiful@example.com>
2026-07-11 19:19:15 +0600

    refactor: remove obsolete dashboard files and database diagnostic scripts

--- check.php
+++
@@ -1,4 +0,0 @@
-<?php
-require 'src/config/database.php';
-$stmt = $db->query('SELECT * FROM quizzes');
-print_r($stmt->fetchAll(PDO::FETCH_ASSOC));
--- check_schema.php
+++
@@ -1,4 +0,0 @@
-<?php
-require __DIR__.'/src/config/database.php';
-$stmt = $db->query('DESCRIBE users');
-print_r($stmt->fetchAll(PDO::FETCH_ASSOC));
